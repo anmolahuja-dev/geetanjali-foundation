ed8994b74b51b2cda3fc7f799e5e1e1eb7cd6c39
= <=>
2022-08-04 16:12:46 +0530

    Chat bot integrated

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Footer from './components/layout/Footer.js';
 import Header from './components/layout/Header.js';
 import Home from './components/layout/Home.js';
 import Posts from './components/layout/Posts.js';
+import Chat from './components/utility/Chat.js';
 function App() {
   return (
     <div className="App ">
+      <Chat/>
       <Header />
       <Home/>
       <About/>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Kommunicate from '@kommunicate/kommunicate-chatbot-plugin';
 import 'bootstrap/dist/css/bootstrap.css';
+
+Kommunicate.init("2ca4ffc6e348b32012c83b95310c57800" , {"popupWidget":true,"automaticChatOpenOnNavigation":true});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
